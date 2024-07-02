524baf2193350d476b05000e3ed6727366c6aae3
유현준 <140572350+YOOHYUNJUNE@users.noreply.github.com>
2024-07-02 11:57:47 +0900

    2

--- 0_react/component_react/src/App.js
+++ 0_react/component_react/src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Comp from './components/Comp';
+import Comp from "./components/Comp";
+// import {Header, Nav, Article} from "./components/Comps"
+import * as Comps from './components/Comps'
+import Introduce from "./components/Introduce";
+
 
 function App() {
+  const {Header, Nav, Article} = Comps;
+
   return (
     <div>
       <Comp></Comp>
-      </div>
+      <Header></Header>
+      <Nav></Nav>
+      {/* props: 부모 컴포넌트가 자식에게 전달하는 값 */}
+      <Article text="안녕하세요"></Article>
+      
+      <Introduce name="유현준" gender="남"></Introduce>
+      <Introduce name="유현준" gender="남"></Introduce>
+      <Introduce name="유현준" gender="남"></Introduce>
+      <Introduce></Introduce>
+    </div>
   );
 }
 
