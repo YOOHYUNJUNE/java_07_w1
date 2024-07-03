1c29c02143dc1b087abb8b6cbc3dcb1c59397302
유현준 <140572350+YOOHYUNJUNE@users.noreply.github.com>
2024-07-03 09:46:17 +0900

    0703_1

--- 0_react/shopping_react/src/components/Product.js
+++ 0_react/shopping_react/src/components/Product.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {useState, useEffect} from "react";
+import ProdInput from "./product/ProdInput";
 
 const Product = () => {
 
@@ -7,16 +8,48 @@ const Product = () => {
 
     const [products, setProducts] = useState([]);
 
-    const getProducts = () => {
-        axios.get('http://localhost:8080/products')
-            .then(res => {return res.data})
-            .then(data => setProducts(data))
+    // 로딩화면 함수
+    const [loading, setLoading] = useState(true);
+    const [err, setErr] = useState(false);
+
+
+
+    // const getProducts = () => {
+    //     axios.get('http://localhost:8080/products') // 데이터를 보냄
+    //         .then(res => res.data) // 성공시
+    //         .then(data => setProducts(data))
+    //         .catch(err => console.error(err))
+    // }
+
+    const getProducts = async() => {
+        try {
+            const res = await axios.get('http://localhost:8080/products')
+            // 로딩화면 구현
+            setLoading(false)
+
+            const data = res.data;
+            setProducts(data);
+        } catch (err) {
+            console.error(err);
+
+            // 에러 구현
+            setErr(true)
+        }
     }
+    
+    useEffect(() => {getProducts()}, []);
+    
 
-    useEffect(() => {
-        getProducts();
-    }, []);
 
+    // 로딩화면 구현
+    if (err) {
+        return <div>에러발생</div>
+    }
+    if (loading) {
+        return <div>로딩 중...</div>
+    }
+    
+    
 
 
     return ( 
@@ -46,7 +79,7 @@ const Product = () => {
                     })
                 }
             </div>
-
+            <ProdInput products={products} setProducts={setProducts}></ProdInput>
         </main>
      );
 }
